2739063b776d5f5642c6a9ceda7926274594e4a1
Nikolay Dobrev <ndobrev@progress.com>
2023-11-28 15:59:06 +0200

    Revert to force dynamic

--- src/app/[...slug]/page.tsx
+++ src/app/[...slug]/page.tsx
@@ -4,7 +4,7 @@ import RenderPage from '@/framework/components/render-page';
 import { pageMetadata, pageStaticParams } from '@/framework/utils';
 import { PageParams } from '@/framework/page-params';
 
-export const dynamic = 'force-static';
+export const dynamic = 'force-dynamic';
 
 export async function generateMetadata({ params, searchParams }: PageParams): Promise<Metadata> {
     return await pageMetadata({ params, searchParams });
